50045cdcbc67306a4711f0da9624cde5acfe5310
Reizo Velasquez <reizovelasquezc@gmail.com>
2025-02-06 17:27:34 -0500

    header Responsive

---
+++ static/js/header.js
@@ -0,0 +1,42 @@
+const headerPopover = document.getElementById('header-popover');
+if (headerPopover) {
+    headerPopover.addEventListener('click', function() {
+        const dropdownContent = document.getElementById('dropdown-content');
+        dropdownContent.style.display = dropdownContent.style.display === 'none' || dropdownContent.style.display === '' ? 'flex' : 'none';
+    });
+
+    headerPopover.addEventListener('blur', function() {
+        const dropdownContent = document.getElementById('dropdown-content');
+        dropdownContent.style.display = 'none';
+    });
+}
+
+const navPopover = document.getElementById('nav-popover');
+const popoverContent = document.getElementById('popover-content');
+
+if (navPopover && popoverContent) {
+    navPopover.addEventListener('click', function(event) {
+        event.stopPropagation();
+        if (window.innerWidth < 1100) {
+            popoverContent.style.display = popoverContent.style.display === 'none' || popoverContent.style.display === '' ? 'flex' : 'none';
+        }
+    });
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth >= 1100) {
+            popoverContent.style.display = 'flex';
+        } else {
+            popoverContent.style.display = 'none';
+        }
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!navPopover.contains(event.target) && !popoverContent.contains(event.target)) {
+            popoverContent.style.display = 'none';
+        }
+    });
+
+    popoverContent.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+}
